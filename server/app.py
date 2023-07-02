7cdfd0cc16398010853086d6e1aafeb825616b39
Mutindi Hope <msndindi@outlook.com>
2023-07-02 15:22:00 +0300

    Completed Assignment

--- server/app.py
+++ server/app.py
@@ -5,6 +5,8 @@ from models  import Customer, db
 
 app = Flask(__name__)
 
+
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 migrate = Migrate(app, db)
